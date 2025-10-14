80ff8d1c80dfbda26c285aff7c2f375af536d796
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2025-10-14 10:36:13 -0400

    re-add extension integration

--- src/blockers/extensionintegration.cpp
+++ src/blockers/extensionintegration.cpp
@@ -11,7 +11,7 @@
 ExtensionIntegration::ExtensionIntegration(QObject *parent)
     : QObject{parent}
 {
-    return; // FIXME: extensionintegration
+    //return; // FIXME: extensionintegration
     ExtensionIntegration::m_pThis = this;
 
     m_firefoxNMManifestDir = QDir::homePath() + "/Library/Application Support/Mozilla/NativeMessagingHosts";
--- src/extensionhost/main_unix.cpp
+++ src/extensionhost/main_unix.cpp
@@ -6,7 +6,7 @@
 #include <array>
 #include <poll.h>
 #include <errno.h>
-
+#include <fstream>
 
 
 
@@ -32,12 +32,17 @@ int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
+    std::ofstream log;
+    log.open("/tmp/focacciat_extensionhost_errlog", std::ios::app | std::ios::out);
+    log << "start" << std::endl;
+
     constexpr size_t maxBufLength = 4096;
-    constexpr char socketClosedMessage[] = "[{\"error\": \"connection closed\"}]";
+    constexpr char socketClosedMessage[] = "{\"type\":\"connection\",\"description\":\"closed\"}";
+    constexpr char socketOpenMessage[] = "{\"type\":\"connection\",\"description\":\"opened\"}";
 
     int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd == -1) {
-        std::cerr << "Socket not opened";
+        log << "Socket not opened: " << strerror(errno) << std::endl;
         return 1;
     }
     struct sockaddr_un addr;
@@ -58,10 +63,10 @@ int main() {
     size_t confstrSize = strlen(tmp_p) + 1;
 #endif
     if (confstrSize == 0) {
-        std::cerr << "Unknown tmpdir path." << std::endl;
+        log << "Unknown tmpdir path." << std::endl;
         return 1;
     } else if (confstrSize > confstrMaxSize) {
-        std::cerr << "path to tmpdir is probably too long" << std::endl;
+        log << "path to tmpdir is probably too long" << std::endl;
         return 1;
     }
 #ifndef __APPLE__
@@ -69,63 +74,75 @@ int main() {
 #endif
     std::strncat(addr.sun_path, socketName, socketNameLen);
 
-    if (connect(sockfd, reinterpret_cast<const struct sockaddr *>(&addr), sizeof(addr)) < 0) {
-        std::cerr << "failed to connect: " << strerror(errno) << std::endl;
-        return 1;
-    }
+    do {
+        // TODO: refactor loop out
+        while (connect(sockfd, reinterpret_cast<const struct sockaddr *>(&addr), sizeof(addr)) < 0) {
+            log << "failed to connect: " << strerror(errno) << std::endl;
+            log << "Retrying in 10s..." << std::endl;
+            // TODO: check if stdin still exists at this point/
+            sleep(10);
+        }
+        constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
+        std::cout.write(reinterpret_cast<const char*>(&size), sizeof(char));
+        std::cout << socketOpenMessage << std::flush;
 
-    constexpr size_t pfds_len = 2;
-    struct pollfd pfds[pfds_len] = {
-        { .fd = STDIN_FILENO, .events = POLLIN },
-        { .fd = sockfd, .events = POLLIN }
-    };
-    short eventsStdin, eventsSock;
+        constexpr size_t pfds_len = 2;
+        struct pollfd pfds[pfds_len] = {
+            { .fd = STDIN_FILENO, .events = POLLIN },
+            { .fd = sockfd, .events = POLLIN }
+        };
+        short eventsStdin, eventsSock;
 
-    std::array<char, maxBufLength> buffer;
+        std::array<char, maxBufLength> buffer;
 
-    while (true) {
-        if (poll(pfds, pfds_len, -1) > 0) {
-            eventsStdin = pfds[0].revents; eventsSock = pfds[1].revents;
+        while (true) {
+            if (poll(pfds, pfds_len, -1) > 0) {
+                eventsStdin = pfds[0].revents; eventsSock = pfds[1].revents;
 
-            if (eventsStdin & (POLLHUP | POLLERR | POLLNVAL)) {
-                std::cerr << "Connection closed." << std::endl;
-                close(sockfd);
-                return 0;
-            }
-            if (eventsSock & (POLLHUP | POLLERR | POLLNVAL)) {
-                std::cerr << "Socket closed. Sending closed message." << std::endl;
-                constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
-                std::cout.write(reinterpret_cast<char*>(size), 4);
-                std::cout << socketClosedMessage << std::flush;
+                if (eventsStdin & (POLLHUP | POLLERR | POLLNVAL)) {
+                    log << "Stdin connection closed." << std::endl;
+                    close(sockfd);
+                    return 0;
+                }
+                if (eventsSock & (POLLHUP | POLLERR | POLLNVAL)) {
+                    log << "Socket closed. Sending closed message." << std::endl;
+                    constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
+                    std::cout.write(reinterpret_cast<const char*>(&size), sizeof(char));
+                    std::cout << socketClosedMessage << std::flush;
 
-                close(sockfd);
-                return 0;
-            }
-            if (eventsStdin & POLLIN) {
-                uint32_t respSize{0};
-                std::cin.read(reinterpret_cast<char*>(&respSize), sizeof(respSize));
-                char* response = static_cast<char*>(malloc(sizeof(char)*respSize));
-                std::cin.read(response, respSize);
-                send(sockfd, response, respSize, 0);
-                free(response);
-            }
-            if (eventsSock & POLLIN) {
-                int bytesRecieved = 0;
-                do {
-                    // DONTWAIT makes sure that if we send exatly maxBufLength chars, it will not block
-                    bytesRecieved = recv(sockfd, &buffer[0], buffer.size(), MSG_DONTWAIT);
-                    // append string from buffer.
-                    if ( bytesRecieved == -1 ) {
-                        // error
-                    } else {
-                        std::copy(buffer.cbegin(), buffer.cbegin() + bytesRecieved, std::ostream_iterator<char>(std::cout));
-                    }
-                } while (bytesRecieved == maxBufLength);
-                std::cout.flush();
+                    close(sockfd);
+                    break;
+                }
+                if (eventsStdin & POLLIN) {
+                    uint32_t respSize{0};
+                    std::cin.read(reinterpret_cast<char*>(&respSize), sizeof(respSize));
+                    char* response = static_cast<char*>(malloc(sizeof(char)*respSize));
+                    std::cin.read(response, respSize);
+                    send(sockfd, response, respSize, 0);
+                    free(response);
+                }
+                if (eventsSock & POLLIN) {
+                    int bytesRecieved = 0;
+                    do {
+                        // DONTWAIT makes sure that if we send exatly maxBufLength chars, it will not block
+                        bytesRecieved = recv(sockfd, &buffer[0], buffer.size(), MSG_DONTWAIT);
+                        // append string from buffer.
+                        if ( bytesRecieved == -1 ) {
+                            // error
+                        } else {
+                            std::copy(buffer.cbegin(), buffer.cbegin() + bytesRecieved, std::ostream_iterator<char>(std::cout));
+                        }
+                    } while (bytesRecieved == maxBufLength);
+                    std::cout.flush();
+                }
             }
         }
-    }
-
-
+        log << "Creating new socket..." << std::endl;
+        sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+        if (sockfd == -1) {
+            log << "Socket not opened: " << strerror(errno) << std::endl;
+            return 1;
+        }
+    } while (true);
     return 0;
 }
