51238ccf2e76e9edbf151600ebbd50467fffa19d
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2025-10-15 21:51:29 -0400

    Make extensionhost not retry after quitted again

--- src/extensionhost/main_unix.cpp
+++ src/extensionhost/main_unix.cpp
@@ -51,8 +51,8 @@ int main(int argc, char* argv[]) {
     log << "start" << std::endl;
 
     constexpr size_t maxBufLength = 4096;
-    constexpr char socketClosedMessage[] = "{\"type\":\"connection\",\"description\":\"closed\"}";
-    constexpr char socketOpenMessage[] = "{\"type\":\"connection\",\"description\":\"opened\"}";
+    constexpr char socketClosedMessage[] = "{\"type\":\"message\",\"action\":\"connection\",\"message\":\"closed\"}";
+    constexpr char socketOpenMessage[] = "{\"type\":\"message\",\"action\":\"connection\",\"message\":\"opened\"}";
 
     int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd == -1) {
@@ -88,81 +88,71 @@ int main(int argc, char* argv[]) {
 #endif
     std::strncat(addr.sun_path, socketName, socketNameLen);
 
-    do {
-        // TODO: refactor loop out
-        while (connect(sockfd, reinterpret_cast<const struct sockaddr *>(&addr), sizeof(addr)) < 0) {
-            log << "failed to connect: " << strerror(errno) << std::endl;
-            log << "Retrying in 10s..." << std::endl;
-            // TODO: check if stdin still exists at this point/
-            sleep(10);
-        }
-        constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
-        std::cout.write(reinterpret_cast<const char*>(&size), sizeof(char));
-        std::cout << socketOpenMessage << std::flush;
+    while (connect(sockfd, reinterpret_cast<const struct sockaddr *>(&addr), sizeof(addr)) < 0) {
+        log << "failed to connect: " << strerror(errno) << std::endl;
+        log << "Retrying in 10s..." << std::endl;
+        // TODO: check if stdin still exists at this point/
+        sleep(10);
+    }
+    constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
+    std::cout.write(reinterpret_cast<const char*>(&size), sizeof(char));
+    std::cout << socketOpenMessage << std::flush;
 
-        constexpr size_t pfds_len = 2;
-        struct pollfd pfds[pfds_len] = {
-            { .fd = STDIN_FILENO, .events = POLLIN },
-            { .fd = sockfd, .events = POLLIN }
-        };
-        short eventsStdin, eventsSock;
+    constexpr size_t pfds_len = 2;
+    struct pollfd pfds[pfds_len] = {
+        { .fd = STDIN_FILENO, .events = POLLIN },
+        { .fd = sockfd, .events = POLLIN }
+    };
+    short eventsStdin, eventsSock;
 
-        std::array<char, maxBufLength> buffer;
+    std::array<char, maxBufLength> buffer;
 
-        while (true) {
-            if (poll(pfds, pfds_len, -1) > 0) {
-                eventsStdin = pfds[0].revents; eventsSock = pfds[1].revents;
+    while (true) {
+        if (poll(pfds, pfds_len, -1) > 0) {
+            eventsStdin = pfds[0].revents; eventsSock = pfds[1].revents;
 
-                if (eventsStdin & (POLLHUP | POLLERR | POLLNVAL)) {
-                    log << "Stdin connection closed." << std::endl;
-                    close(sockfd);
-                    return 0;
-                }
-                if (eventsSock & (POLLHUP | POLLERR | POLLNVAL)) {
-                    log << "Socket closed. Sending closed message." << std::endl;
-                    constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
-                    std::cout.write(reinterpret_cast<const char*>(&size), sizeof(char));
-                    std::cout << socketClosedMessage << std::flush;
+            if (eventsStdin & (POLLHUP | POLLERR | POLLNVAL)) {
+                log << "Stdin connection closed." << std::endl;
+                close(sockfd);
+                return 0;
+            }
+            if (eventsSock & (POLLHUP | POLLERR | POLLNVAL)) {
+                log << "Socket closed. Sending closed message." << std::endl;
+                constexpr uint32_t size = sizeof(socketClosedMessage)/sizeof(char);
+                std::cout.write(reinterpret_cast<const char*>(&size), sizeof(char));
+                std::cout << socketClosedMessage << std::flush;
 
-                    close(sockfd);
-                    break;
-                }
-                if (eventsStdin & POLLIN) {
-                    if (nldelstdin) {
-                        uint32_t respSize{0};
-                        std::cin.read(reinterpret_cast<char*>(&respSize), sizeof(respSize));
-                        char* response = static_cast<char*>(malloc(sizeof(char)*respSize));
-                        std::cin.read(response, respSize);
-                        send(sockfd, response, respSize, 0);
-                        free(response);
-                    } else {
-                        std::string result;
-                        std::getline(std::cin, result);
-                        send(sockfd, result.c_str(), result.size(), 0);
-                    }
-                }
-                if (eventsSock & POLLIN) {
-                    int bytesRecieved = 0;
-                    do {
-                        // DONTWAIT makes sure that if we send exatly maxBufLength chars, it will not block
-                        bytesRecieved = recv(sockfd, &buffer[0], buffer.size(), MSG_DONTWAIT);
-                        // append string from buffer.
-                        if ( bytesRecieved == -1 ) {
-                            // error
-                        } else {
-                            std::copy(buffer.cbegin(), buffer.cbegin() + bytesRecieved, std::ostream_iterator<char>(std::cout));
-                        }
-                    } while (bytesRecieved == maxBufLength);
-                    std::cout.flush();
+                close(sockfd);
+                return 0;
+            }
+            if (eventsStdin & POLLIN) {
+                if (nldelstdin) {
+                    uint32_t respSize{0};
+                    std::cin.read(reinterpret_cast<char*>(&respSize), sizeof(respSize));
+                    char* response = static_cast<char*>(malloc(sizeof(char)*respSize));
+                    std::cin.read(response, respSize);
+                    send(sockfd, response, respSize, 0);
+                    free(response);
+                } else {
+                    std::string result;
+                    std::getline(std::cin, result);
+                    send(sockfd, result.c_str(), result.size(), 0);
                 }
             }
+            if (eventsSock & POLLIN) {
+                int bytesRecieved = 0;
+                do {
+                    // DONTWAIT makes sure that if we send exatly maxBufLength chars, it will not block
+                    bytesRecieved = recv(sockfd, &buffer[0], buffer.size(), MSG_DONTWAIT);
+                    // append string from buffer.
+                    if ( bytesRecieved == -1 ) {
+                        // error
+                    } else {
+                        std::copy(buffer.cbegin(), buffer.cbegin() + bytesRecieved, std::ostream_iterator<char>(std::cout));
+                    }
+                } while (bytesRecieved == maxBufLength);
+                std::cout.flush();
+            }
         }
-        log << "Creating new socket..." << std::endl;
-        sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (sockfd == -1) {
-            log << "Socket not opened: " << strerror(errno) << std::endl;
-            return 1;
-        }
-    } while (true);
-    return 0;
+    }
 }
